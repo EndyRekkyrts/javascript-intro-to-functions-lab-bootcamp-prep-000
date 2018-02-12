1b924f34b56cd96604c12ce74807aef6a228b978
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-12 23:07:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
   }
-  
-function sayHiToGrandma(string) {
-  return string.toUpperCase();
-  }
+
 function sayHiToGrandma(string) {
   return string.toLowerCase();
+  }
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase();
   }
 
 function sayHiToGrandma(string) {
